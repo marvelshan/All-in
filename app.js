208426d0e47fbc4f18b63aff99149b4c65528df4
Zaki <reborn7875@gmail.com>
2023-11-18 15:47:50 +0800

    feat: add function websocket

--- app.js
+++ app.js
@@ -1,14 +1,35 @@
 import express from 'express';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
 import oddsRouter from './routes/odds.js';
+import gameRouter from './routes/game.js';
+// import userRouter from './routes/user.js';
+// import client from './utils/cache.js';
 
 const app = express();
+const httpServer = createServer(app);
 
 app.set('view engine', 'ejs');
 app.use(express.json());
-
+app.use(express.static('public'));
 app.use('/odds', oddsRouter);
+app.use('/game', gameRouter);
+// app.use('/user', userRouter);
+
+const io = new Server(httpServer, {});
+
+let likes = 0;
+io.on('connection', (socket) => {
+  console.log('connect');
+  socket.emit('likeupdated', likes);
+  socket.on('liked', () => {
+    likes += 1;
+    socket.emit('likeupdated', likes);
+    socket.broadcast.emit('likeupdated', likes);
+  });
+});
 
 const port = 3000;
-app.listen(port, () => {
+httpServer.listen(port, () => {
   console.log(`Server is working on ${port}`);
 });
