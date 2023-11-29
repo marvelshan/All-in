00bf5318ec0f731e09c0bc46def88f04380ae3c4
zaki <reborn7875@gmail.com>
2023-11-29 21:41:52 +0800

    fix: Modify game restart time

--- utils/gameWorker.js
+++ utils/gameWorker.js
@@ -21,25 +21,22 @@ async function gameEnd(event, id) {
         data.betting_point * parseFloat(data.betting_odds) * -1;
       await userModel.changeUserPoint(winningPoint, data.member_id);
     });
-    setTimeout(
-      () => {
-        const nextId = parseInt(id, 10);
-        axios
-          .post('/game/start', {
-            id: nextId,
-          })
-          .then((response) => {
-            console.log(response.data);
-          })
-          .catch((error) => {
-            console.error('Error making HTTP request:', error);
-          });
-        // Cuz game emulator will repeat so web need to
-        // delete bet information to make last game bet information will not influence next game
-        userModel.deleteBetInfor(id);
-      },
-      5 * 60 * 1000,
-    );
+    setTimeout(() => {
+      const nextId = parseInt(id, 10);
+      axios
+        .post('https://ygolonhcet.online/game/start', {
+          id: nextId,
+        })
+        .then((response) => {
+          console.log(response.data);
+        })
+        .catch((error) => {
+          console.error('Error making HTTP request:', error);
+        });
+      // Cuz game emulator will repeat so web need to
+      // delete bet information to make last game bet information will not influence next game
+      userModel.deleteBetInfor(id);
+    }, 1000);
   }
 }
 
@@ -53,26 +50,30 @@ async function gamePopFromRedis() {
   const id = await client.lpop('game');
   const startTimeInRedis = await client.lpop('startTime');
   const numberInRedis = await client.lpop('number');
-  games.push(id);
-  startTime.push(startTimeInRedis);
-  number.push(numberInRedis);
+  if (!games.includes(id)) {
+    games.push(id);
+    startTime.push(startTimeInRedis);
+    number.push(numberInRedis);
+  }
   gameSetTimeOut(id);
 }
 
 async function putGameEventIntoRedis(id) {
   const gameEvent = await client.lpop(`gameRedis${id}`);
   const event = JSON.parse(gameEvent);
-  const timeDiff =
-    (new Date(event.wallclk) - new Date(startTime[games.indexOf(id)])) / 10;
-  setTimeout(async () => {
-    await client.publish('game', id);
-    await client.set(`game${id}`, JSON.stringify(event));
-    const nextGameId = await client.lindex('game', 0);
-    if (nextGameId !== null) {
-      gamePopFromRedis();
-    }
-    gameEnd(event, id);
-  }, timeDiff);
+  if (event !== null) {
+    const timeDiff =
+      (new Date(event.wallclk) - new Date(startTime[games.indexOf(id)])) / 10;
+    setTimeout(async () => {
+      await client.publish('game', id);
+      await client.set(`game${id}`, JSON.stringify(event));
+      const nextGameId = await client.lindex('game', 0);
+      if (nextGameId !== null) {
+        gamePopFromRedis();
+      }
+      gameEnd(event, id);
+    }, timeDiff);
+  }
 }
 
 gamePopFromRedis();
