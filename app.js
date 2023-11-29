be887f9c4aedfcd4b2632635425f3e9d38475993
zaki <reborn7875@gmail.com>
2023-11-29 21:35:47 +0800

    feat:Add node cluster on server

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ import express from 'express';
 import { createServer } from 'http';
 import cookieParser from 'cookie-parser';
 import swaggerUi from 'swagger-ui-express';
+import * as fs from 'fs';
+import path from 'path';
+import cluster from 'cluster';
+import os from 'os';
 
-import swaggerDocument from './swagger-output.json' assert { type: 'json' };
 import oddsRouter from './routes/odds.js';
 import gameRouter from './routes/game.js';
 import userRouter from './routes/user.js';
@@ -13,20 +16,34 @@ import initializeWebSocket from './utils/socket.js';
 
 const app = express();
 const httpServer = createServer(app);
+const swaggerDocument = JSON.parse(
+  fs.readFileSync(`${path.resolve()}/swagger-output.json`).toString(),
+);
+const cCPUs = os.cpus().length;
 
-app.use(cookieParser());
-app.use(express.json());
-app.use(express.static('public'));
-app.use('/odds', oddsRouter);
-app.use('/game', gameRouter);
-app.use('/user', userRouter);
-app.use('/topUp', chargeRouter);
-app.use('/rank', rankRouter);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+if (cluster.isMaster) {
+  for (let i = 0; i < cCPUs; i += 1) {
+    cluster.fork();
+  }
+  cluster.on('exit', (worker) => {
+    console.log(`Worker ${worker.process.pid} died. Restarting...`);
+    cluster.fork();
+  });
+} else {
+  app.use(cookieParser());
+  app.use(express.json());
+  app.use(express.static('public'));
+  app.use('/odds', oddsRouter);
+  app.use('/game', gameRouter);
+  app.use('/user', userRouter);
+  app.use('/topUp', chargeRouter);
+  app.use('/rank', rankRouter);
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-initializeWebSocket(httpServer);
+  initializeWebSocket(httpServer);
 
-const port = 3000;
-httpServer.listen(port, () => {
-  console.log(`Server is working on ${port}`);
-});
+  const port = 3000;
+  httpServer.listen(port, () => {
+    console.log(`Server is working on ${port}`);
+  });
+}
