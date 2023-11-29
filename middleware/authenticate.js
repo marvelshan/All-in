271172ee4c863351726728dd7f96a75946d3abc4
zaki <reborn7875@gmail.com>
2023-11-29 21:39:40 +0800

    test: Add console.time to calculate bet per time

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -2,7 +2,9 @@ import jwt from 'jsonwebtoken';
 import * as model from '../model/user.js';
 
 const authenticate = async (req, res, next) => {
+  console.time('Authorization');
   try {
+    console.time('authenticate');
     const tokenInHeaders = req.get('Authorization');
     const token =
       tokenInHeaders?.replace('Bearer ', '') || req.cookies.jwtToken;
@@ -10,7 +12,10 @@ const authenticate = async (req, res, next) => {
       res.status(401).json({ errors: 'invalid token' });
       return;
     }
+    console.log(req.cookies.jwtToken);
     const privateKey = process.env.JWT_KEY;
+    console.timeEnd('Authorization');
+    console.time('JWTverify');
     try {
       const userInformation = jwt.verify(token, privateKey);
       const { userId, name, email } = userInformation.user;
@@ -23,12 +28,16 @@ const authenticate = async (req, res, next) => {
     } catch (error) {
       return res.status(403).send('wrong token');
     }
+    console.timeEnd('JWTverify');
+    console.time('findUser');
     try {
       // check later
       await model.findUser(req.body.email);
     } catch (error) {
       return res.status(403).send('wrong account');
     }
+    console.timeEnd('findUser');
+    console.timeEnd('authenticate');
     next();
   } catch (error) {
     res.status(500).send(`middleware authenticate is error on ${error}`);
